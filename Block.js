58c7ac76f0105467b090441d8a8ea98fd6f5bb65
Hank Kim <hank29206880@gmail.com>
2023-03-31 13:41:24 +0900

    블럭 shoaw

--- Block.js
+++ Block.js
@@ -12,5 +12,19 @@ export class Block {
     ctx.beginPath();
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.fill();
+    ctx.fillStyle = "#190f3a";
+    ctx.beginPath();
+    ctx.moveTo(this.maxX, this.maxY);
+    ctx.lineTo(this.maxX - 60, this.maxY + 40);
+    ctx.lineTo(this.x - 60, this.maxY + 40);
+    ctx.lineTo(this.x, this.maxY);
+    ctx.fill();
+    ctx.fillStyle = "#9d0919";
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x, this.maxY);
+    ctx.lineTo(this.x - 60, this.maxY + 40);
+    ctx.lineTo(this.x - 60, this.maxY + 40 - this.height);
+    ctx.fill();
   }
 }
